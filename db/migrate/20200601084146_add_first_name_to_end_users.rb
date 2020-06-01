d2c697586965b70320ae9bd515544d990d5a7bf5
shuichi <shu.heihei1209@gmail.com>
2020-06-01 19:54:44 +0900

    user_davise

---
+++ db/migrate/20200601084146_add_first_name_to_end_users.rb
@@ -0,0 +1,12 @@
+class AddFirstNameToEndUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :end_users, :first_name, :string
+    add_column :end_users, :family_name, :string
+    add_column :end_users, :first_name_kana, :string
+    add_column :end_users, :family_name_kana, :string
+    add_column :end_users, :phone_number, :string
+    add_column :end_users, :post_code, :string
+    add_column :end_users, :address, :string
+    add_column :end_users, :is_active, :boolean, dafalt: true
+  end
+end
