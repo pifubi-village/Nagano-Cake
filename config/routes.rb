ca36903b07abaaddb265e7fa47dfa2b31f6f7349
masataka <hanayoriinko@gmail.com>
2020-06-01 17:52:48 +0900

    add_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
+  
+
+  namespace :admin do
+   
+    get 'homes/top'
+    resources :genres,only: [:index,:show,:edit,:create,:update]
+    resources :orders,only: [:index,:show,:update]
+    patch 'order_products' #制作ステータスのところです。
+    resources :end_users,only: [:index,:show,:edit,:update]
+    resources :products
+  end
+
+  root 'homes#top'
+  resources :orders,only: [:index,:show,:new,:create,]
+  post 'orders/confirm'
+  get 'orders/complete'
+  resources :cart_products,only: [:index,:edit,:update,:destroy]
+  post 'cart_products/add_product'
+  delete 'cart_products/destroy_all'
+  resources :products,only: [:index,:show]
+  resources :addresses
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
