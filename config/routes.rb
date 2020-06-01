9909288fd45a8be97504bb9e2891736b56a6abda
masataka michikawa <62253140+masataka3@users.noreply.github.com>
2020-06-01 18:47:07 +0900

    Revert "Add controller"

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,5 @@
 Rails.application.routes.draw do
-
-  namespace :admin do
-    devise_for :admins
-    get 'homes/top'
-    resources :genres,only: [:index,:show,:edit,:create,:update]
-    resources :orders,only: [:index,:show,:update]
-    patch 'order_products' #制作ステータスのところです。
-    resources :end_users,only: [:index,:show,:edit,:update]
-    resources :products
-  end
-
-  root 'homes#top'
-  resources :orders,only: [:index,:show,:new,:create,]
-  post 'orders/confirm'
-  get 'orders/complete'
-  resources :cart_products,only: [:index,:edit,:update,:destroy]
-  post 'cart_products/add_product'
-  delete 'cart_products/destroy_all'
-  resources :products,only: [:index,:show]
-  resources :addresses
+  devise_for :admins
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
