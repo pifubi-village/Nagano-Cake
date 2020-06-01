b63237909cb67563de8a9459a4ae65f9f91cf57e
yuping-xiao <yupingxiao94@gmail.com>
2020-06-01 20:59:43 +0900

    modified models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :adresses, dependent: :destroy
+  has_many :orders, dependent: :destroy
+  has_many :cart_products, dependent: :destroy
+  
 end
