5e08a43289b8e67bbae8c04a68075a699ddc1d42
yuping-xiao <yupingxiao94@gmail.com>
2020-06-02 13:50:40 +0900

    modified models

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -3,4 +3,10 @@ class OrderProduct < ApplicationRecord
 	belongs_to :order
 	belongs_to :product
 
+	enum production_status:{
+		not_available: 0,
+		waiting: 1,
+		preparing: 2,
+		completed: 3
+	}
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,10 @@ class Product < ApplicationRecord
 	has_many :order_products, dependent: :destroy
 
 	belongs_to :genre
+
+	enum selling_status:{
+		selling: 0,
+		stopping: 1,
+		finished: 2
+	}
 end
