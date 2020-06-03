55e4787d210a76d0606b79294fbf63adccccd416
masataka <hanayoriinko@gmail.com>
2020-06-03 13:16:48 +0900

    add address

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,14 +8,22 @@ class OrdersController < ApplicationController
   end
 
   def new
+    
   end
 
   def create
   end
 
   def confirm
+    @order = Order.find(params[:id])
   end
 
   def complete
+    @order = Order.find(params[:id])
+  end
+
+  private
+  def order_params
+    params.require(:order).permit()
   end
 end
