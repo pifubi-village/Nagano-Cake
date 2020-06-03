e2a92185c0147542363769c1113960df782285bd
bet-better36 <61450126+bet-better36@users.noreply.github.com>
2020-06-03 20:27:19 +0900

    add_genre-1

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_06_02_084636) do
-
+ActiveRecord::Schema.define(version: 2020_06_03_104642) do
 
   create_table "addresses", force: :cascade do |t|
     t.integer "end_user_id"
@@ -70,15 +68,16 @@ ActiveRecord::Schema.define(version: 2020_06_02_084636) do
     t.string "post_code"
     t.string "address"
     t.boolean "is_active"
+    t.string "family_name"
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
 
   create_table "genres", force: :cascade do |t|
-    t.integer "name"
     t.boolean "is_active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "order_products", force: :cascade do |t|
@@ -111,18 +110,6 @@ ActiveRecord::Schema.define(version: 2020_06_02_084636) do
     t.integer "selling_status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
