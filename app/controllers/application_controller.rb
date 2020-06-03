2f0c33d26c66689e49248fd76238f2339138738b
masataka <hanayoriinko@gmail.com>
2020-06-03 20:20:10 +0900

    所々エラーが出ます

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,16 +6,3 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up,keys: [:first_name, :first_name_kana, :family_name, :family_name_kana, :phone_number, :email, :post_code, :address])
   end
 end
-
-
-private
-	def current_cart
-
-		Cart.find(session[:cart_id])
-
-		rescue ActiveRecord::RecordNotFound
-			cart = Cart.create
-			session[:cart_id] = cart.id
-			cart
-	end
-end
