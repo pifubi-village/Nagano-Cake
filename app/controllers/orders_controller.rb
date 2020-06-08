4d5ca89db204aa8a8c2b3381d33af08a677b9f74
masataka <hanayoriinko@gmail.com>
2020-06-08 19:03:15 +0900

    少し進んだ

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -29,25 +29,27 @@ class OrdersController < ApplicationController
     end
 
   def confirm
-    #binding.pry
+
     @cart_products = CartProduct.all
     @orders = Order.all
-    session[:order]=params[:order][:payment_method]
-    session[:order]=params[:order][:address]
-    session[:order]=params[:order][:post_code]
-    session[:order]=params[:order][:name]
+    session[:order] = params[:order]
+    #binding.pry
 
-   # if  order_id "address_1"
-    #  order.address = 'address_1'
-   # elsif
-     # 'address_2'
-   # else
-     # 'address_3'
-    #  address_1 = "新しいお届け先"
-    #  address_2 = "登録済みの住所"
-    #  address_3= "新しいお届け先"
-   # end
-   end
+    @order =Order.new
+    if params[:order][:payment_method] == "0"
+      @order.payment_method = "cash"
+    else @order.payment_method = "クレジット"
+    end
+
+    @order = Order.new
+    if params[:order][:address_status] == "0"
+      @order.address = current_end_user.address
+    elsif  params[:order][:address_status] == "1"
+      #@order.address = addresses.current_end_user.address_id.
+
+    elsif address_status == "2"
+    end
+  end
 
   def complete
     @order = Order.new(Order_params)
