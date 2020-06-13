4333ffdfd615d4164db7f7843e710a2335063477
vagrant <vagrant@localhost.localdomain>
2020-06-13 06:23:51 +0000

    layout

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,13 @@
 class HomesController < ApplicationController
 	def top
-		@products = Product.all
-		@products = Product.page(params[:page]).per(4)
+	 if params[:genre_id]
+        @products = Product.joins(:genre).where(genres: {is_active: true, id: params[:genre_id]}).page(params[:page]).per(5).reverse_order
+    else
+  	    @products = Product.joins(:genre).where(genres: {is_active: true}).page(params[:page]).per(5).reverse_order
+    end
+	    @genres = Genre.all
 	end
+
 	def about
 	end
 end
