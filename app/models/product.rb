0a5009793d3f68add95db6a58781499106349a8c
masataka michikawa <masataaaka3@icloud.com>
2020-07-21 12:31:49 +0000

    delete-favorite

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,6 @@ class Product < ApplicationRecord
 	has_many :order_products, dependent: :destroy
 	has_many :favorites, dependent: :destroy
 
-	def favorited_by?(end_user)
-		favorites.where(end_user_id: end_user.id).exists?
-	end
 
 	belongs_to :end_user
 	belongs_to :genre
